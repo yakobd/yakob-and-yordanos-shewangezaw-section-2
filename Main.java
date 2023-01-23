4e8ece5355b39c134fed43e980cceaee7e83cde2
Yakob Dereje <yakobdereje.yd@gmail.com>
2023-01-22 17:16:35 -0800

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+// string
+
+    public static void main(String[] args) {
+    String fName = " yakob dereje negash";
+        System.out.println(" before translaton = " + fName);
+    fName.toUpperCase();
+        System.out.println(" after trnslation = " + fName.toUpperCase());
+    }
+}
